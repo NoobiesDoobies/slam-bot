e7187c781379c92b11c560b317ba1f064039e6aa
NoobiesDoobies <carlioseryan20@gmail.com>
2024-08-30 11:20:40 +0700

    fix depth camera orientation with camera_link_optical

--- launch/rtabmap.launch.py
+++ launch/rtabmap.launch.py
@@ -1,7 +1,12 @@
 import os
 
+<<<<<<< HEAD
 from ament_index_python.packages import get_package_share_directory
 
+=======
+
+from ament_index_python.packages import get_package_share_directory
+>>>>>>> e5fbb46 (fix depth camera orientation with camera_link_optical)
 
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, TimerAction, ExecuteProcess
@@ -10,6 +15,7 @@ from launch.substitutions import Command, LaunchConfiguration, PythonExpression
 from launch.actions import RegisterEventHandler, DeclareLaunchArgument
 from launch.event_handlers import OnProcessStart, OnProcessExit
 
+<<<<<<< HEAD
 from launch_ros.actions import Node
 
 
@@ -37,4 +43,29 @@ def generate_launch_description():
     # Launch!
     return LaunchDescription([
         rtabmap
+=======
+def generate_launch_description():
+    return LaunchDescription([
+
+
+
+         IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('rtabmap_launch'),'launch','rtabmap.launch.py'
+                )]), launch_arguments={                
+                    'rtabmap_args': "--delete_db_on_start" ,
+                    'rgb_topic': "/diffbot/front/image_raw",
+                    'depth_topic': "/diffbot/front/depth/image_raw",
+                    'camera_info_topic': "/diffbot/front/camera_info",
+                    'frame_id': "base_link",
+                    'approx_sync': "false",
+                    'wait_imu_to_init': "true",
+                    'imu_topic': "/diffbot/imu",
+                    'qos': "1",
+                    'rviz': "true"
+                }.items()
+
+        )   
+
+>>>>>>> e5fbb46 (fix depth camera orientation with camera_link_optical)
     ])
---
+++ launch/teleop_keyboard.launch.py
@@ -0,0 +1,13 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+
+def generate_launch_description():
+    return LaunchDescription([
+        ExecuteProcess(
+            cmd=['xterm', '-e', 'ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args -p stamped:=true --remap cmd_vel:=/diffbot_base_controller/cmd_vel'],
+            output='screen',
+            shell=True
+        )
+    ])
